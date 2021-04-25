40fa4103f0c2f05076df9c86b9c6203b817ecba4
DefineDan <63105659+DefineDan@users.noreply.github.com>
2021-04-25 13:20:52 +0200

    All challenges + bonus

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -14,6 +14,7 @@ public class PlayerControllerX : MonoBehaviour
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
+    private float boost_speed = 20;
 
     public ParticleSystem boostParticles;
     
@@ -32,6 +33,14 @@ public class PlayerControllerX : MonoBehaviour
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
 
+        // turbo boost effect
+        boostParticles.transform.position = transform.position;
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRb.AddForce(focalPoint.transform.forward * boost_speed, ForceMode.Impulse);
+            boostParticles.Play();
+        }
+
     }
 
     // If Player collides with powerup, activate powerup
@@ -42,6 +51,8 @@ public class PlayerControllerX : MonoBehaviour
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
+            CancelInvoke("PowerupCooldown");
+            Invoke("PowerupCooldown", powerUpDuration);
         }
     }
 
@@ -57,7 +68,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position; 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
