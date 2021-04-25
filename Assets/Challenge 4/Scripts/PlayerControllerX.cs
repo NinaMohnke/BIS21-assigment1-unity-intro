47f29d1757e1c5a7e226d8bb5e8d4ecba44cc8ca
Toalaah <samuel.kunst@gmail.com>
2021-04-25 15:48:15 +0200

    Fix core challenge bugs

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -42,6 +42,8 @@ public class PlayerControllerX : MonoBehaviour
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
+            CancelInvoke("PowerupCooldown"); // if we previously picked up an powerup
+            Invoke("PowerupCooldown", powerUpDuration);
         }
     }
 
@@ -57,7 +59,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
